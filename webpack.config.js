9f32a1a73bb295a7e779215a58cee2dfee8861bc
leowang <rfv7855659@gmail.com>
2019-07-30 23:50:43 +0800

    新增css-loader與style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,20 @@ let path = require('path')
 module.exports = {
     context: path.resolve(__dirname, './src'),
     entry: {
-       index: './index.js',
-       about: './about.js',
+       index: './js/index.js',
+       about: './js/about.js',
     },
     output: {
         path: path.resolve(__dirname,'./dist'),
         filename:'[name].bundle.js'
+    },
+    module:{
+        rules:[
+            {
+                test: /\.css$/,
+                use:['style-loader','css-loader']
+            }
+        ]
     }
+
 }
