488efd08ac12bbd67f015f790610ae39fcc7a9e2
leowang <rfv7855659@gmail.com>
2019-08-15 22:04:58 +0800

    使用image-webpack-loader對圖片進行壓縮

--- webpack.config.js
+++ webpack.config.js
@@ -60,7 +60,31 @@ module.exports = {
                       name:'[path][name].[ext]?[hash:8]'
                     },
                   },
-                ],
+                  {
+                    loader: 'image-webpack-loader',
+                    options: {
+                      mozjpeg: {
+                        progressive: true,
+                        quality: 65
+                      },
+                      // optipng.enabled: false will disable optipng
+                      optipng: {
+                        enabled: false,
+                      },
+                      pngquant: {
+                        quality: '65-90',
+                        speed: 4
+                      },
+                      gifsicle: {
+                        interlaced: false,
+                      },
+                      // the webp option will enable WEBP
+                      webp: {
+                        quality: 75
+                      }
+                    }
+                  }
+                ]
             },
         ]
     },
